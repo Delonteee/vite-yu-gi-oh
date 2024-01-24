6c51fd96d6495dc8f8b90b8d94cfd63a2548178a
Davide Gasparotti <56916853+Delonteee@users.noreply.github.com>
2024-01-24 12:29:42 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    data() {
+      return { count: 0 }
+    },
+};
+</script>
+
+<template>
+  <div>
+    <h1>
+      Template Vite
+    </h1>
+    <p>
+      {{count}}
+    </p>
+    <button @click="count++">
+      Incrementa counter
+    </button>
+  </div>
+</template>
+
+<style scoped>
+  /* STILE CSS CON SCOPE A QUESTO FILE */
+</style>
